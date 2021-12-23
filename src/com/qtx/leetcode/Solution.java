032c8a1953fec325257b527faa576ddb7944fb87
QianTingXu <1102214883@qq.com>
2021-12-23 16:15:04 +0800

    feat:Array practice questions

--- src/com/qtx/leetcode/Solution.java
+++ src/com/qtx/leetcode/Solution.java
@@ -2,8 +2,11 @@ package com.qtx.leetcode;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 /**
  * @Author: QTX
@@ -60,5 +63,74 @@ public class Solution {
     public void maxSubArrayTest() {
         int[] ints = {-1, 0, -2};
         System.out.println(maxSubArray(ints));
+    }
+
+    /**
+     * 俩数之和
+     *
+     * @param nums   数组
+     * @param target 目标值
+     *
+     * @return 俩数位置
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] ints = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ints[0] = i;
+                    ints[1] = j;
+                }
+            }
+        }
+        return ints;
+    }
+
+    /**
+     * 俩数之和
+     *
+     * @param nums   数组
+     * @param target 目标值
+     *
+     * @return 俩数位置
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        HashMap<Object, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp), i};
+            }
+            map.put(temp, i);
+        }
+        return null;
+    }
+
+    @Test
+    public void twoSumTest() {
+        int[] ints = {5, 5, 99, 4, 7, 1};
+        int[] twoSum = twoSum(ints, 8);
+        System.out.println(Arrays.toString(twoSum));
+    }
+
+    /**
+     * 合并两个有序数组
+     *
+     * @param nums1 数组1
+     * @param m     截取长度
+     * @param nums2 数组2
+     * @param n     截取长度
+     */
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+        return nums1;
+    }
+
+    @Test
+    public void mergeTest() {
+        int[] ints = {1, 2, 3, 0, 0, 0, 0};
+        int[] ints1 = {2, 4, 8, 9};
+        System.out.println(Arrays.toString(merge(ints, 3, ints1, 4)));
     }
 }
