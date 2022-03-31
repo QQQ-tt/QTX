3cd052822bfff3d46e74ab1741f85edc88c8d931
QQQtx <1102214883@qq.com>
2022-03-31 17:32:43 +0800

    feat:枚举

---
+++ src/com/qtx/task/ActionType.java
@@ -0,0 +1,16 @@
+package com.qtx.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 行为枚举
+ *
+ * @Author: QTX
+ * @Date: 2022/3/23
+ */
+@AllArgsConstructor
+@Getter
+public enum ActionType {
+    
+}
